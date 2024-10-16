5c09e1c9c74562c3d42aaac5f670ff3f9126e2cf
littlepony <3011726384@qq.com>
2024-10-17 00:15:03 +0800

    测试

--- Chicken/Chicken.cpp
+++ Chicken/Chicken.cpp
@@ -3,98 +3,76 @@
 
 class Chicken
 {
-private:
-    int age = 24;      
-    char *name = nullptr;   
-
-public:
-    Chicken() {};
-
-    Chicken(int _age) : age(_age) {};
-
-    Chicken(int _age, const char *_name) : age(_age)
-    {
-        int len = strlen(_name) + 1;
-        name = new char[len];
-        for (int i = 0; i < len; i++)
-            name[i] = _name[i];
-    }
-
-    Chicken(const Chicken &other)
-    {
-        age = other.age;
-        if (other.name != nullptr)
+    private:
+        int age = 24;
+        char *name = nullptr;
+    public:
+        Chicken(){};
+        Chicken(int _age) : age(_age) {};
+        Chicken(int _age, const char *_name) : age(_age) 
         {
-            int len = strlen(other.name) + 1;
+            int len = strlen(_name) + 1;
             name = new char[len];
             for (int i = 0; i < len; i++)
-                name[i] = other.name[i];
-        }
-        else
+                name[i] = _name[i];
+        };
+        Chicken(const Chicken &_chicken)
         {
-            name = nullptr;
+            *this = _chicken; 
         }
-    }
-
-    Chicken &operator=(const Chicken &other)
-    {
-
-        if (this != &other)
+        Chicken& operator= (const Chicken& other){
+            setAge(other.age);
+            setName(other.name);
+            return *this;
+        }
+        
+        ~Chicken()
         {
             if (name != nullptr)
-            {
                 delete[] name;
-                name = nullptr;
-            }
-
-            age = other.age;
-
-            if (other.name != nullptr)
-            {
-                int len = strlen(other.name) + 1;
-                name = new char[len];
-                for (int i = 0; i < len; i++)
-                    name[i] = other.name[i];
-            }
-            else
-            {
-                name = nullptr;
-            }
         }
-        return *this;
-    }
-
-    ~Chicken()
-    {
-        if (name != nullptr)
+        void setAge(int _age)
         {
-            delete[] name;
+            age = _age;
         }
-    }
+        void setName(const char *_name)
+        {
+            if(name != nullptr)
+                delete[] name;
+            int len = strlen(_name) + 1;
+            name = new char[len];
+            for (int i = 0; i < len; i++)
+                name[i] = _name[i];
+        }
 
-    void setAge(int _age)
-    {
-        age = _age;
-    }
+        const char *getName() const
+        {
+            return name;
+        }
 
-    void setName(const char *_name)
-    {
-        if (name != nullptr)
-            delete[] name;
-
-        int len = strlen(_name) + 1;
-        name = new char[len];
-        for (int i = 0; i < len; i++)
-            name[i] = _name[i];
-    }
-
-    const char *getName() const
-    {
-        return name;
-    }
-
-    const int &getAge() const
-    {
-        return age;
-    }
+        const int &getAge() const
+        {
+            return age;
+        }
 };
+
+int main(){ 
+auto print = [](const Chicken &c){ 
+std::cout << "Hi, everyone! My name is " << c.getName() << ", I am " << c.getAge() << " years old." << std::endl; 
+    }; 
+Chicken c(24, "Kunkun"); 
+    print(c); 
+    Chicken d; 
+    d = c; 
+    print(d); // 测试【赋值运算符】是否正确，能正确输出给 20 分 
+    Chicken a = c; 
+    print(a); // 测试【赋值构造函数】是否正确，能正确输出给 20 分 
+    c.setName("Xukun Cai"); 
+    print(c); 
+    print(a); 
+    print(d); // 测试是否为【深度复制】，本行与上两行能正确输出给 20 分 
+    Chicken b; 
+    b = d = c; 
+    print(b); 
+    print(d); // 测试【连续赋值】功能，本行与上一行能正确输出给 20 分 
+    return 0;}
